344c4bae35cef9a3679f05c7024fded9b3315371
Sargis <sargis@live.ca>
2020-08-06 22:28:07 -0400

    remove broken games

--- Portfolio_test/TypeScript/BloodBoil2D/Scripts/managers/game.ts
+++
@@ -1,9 +0,0 @@
-module managers {
-    export class Game {
-        public static stage: createjs.Stage;
-        public static assetManager: createjs.LoadQueue;
-        public static currentScene: number;
-        public static keyboardManager: managers.Keyboard;
-        public static textureAtlas: createjs.SpriteSheet;
-    }
-}
